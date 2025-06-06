df4c6ef961ce8d615cf2757c9d26ae0cddf9b9a3
Jakub Zych <jzych89@gmail.com>
2025-06-07 01:14:00 +0200

    Add cmd input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+
+    print!("Insert expression: ");
+
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+
+    println!("Input: ,{}", buffer);
 }
